aea1ab67a32e2a81e8abf3d08b982a31ac91e31d
Elia Perantoni <perantonielia0@gmail.com>
2021-04-26 09:33:47 +0200

    Moved stuff about

--- main.cpp
+++ main.cpp
@@ -132,20 +132,24 @@ int main() {
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     unsigned int EBO;
     glGenBuffers(1, &EBO);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 #ifdef WIREFRAME
