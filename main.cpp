4eefa8861de5bef8ea883a85b457712db072eeaa
Elia Perantoni <perantonielia0@gmail.com>
2021-04-26 16:34:58 +0200

    Fix using texture id instead of GL_TEXTURE_2D

--- main.cpp
+++ main.cpp
@@ -44,10 +44,10 @@ int main() {
 
         glBindTexture(GL_TEXTURE_2D, texture1);
 
-        glTexParameteri(texture1, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(texture1, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(texture1, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(texture1, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         int width, height, nrChannels;
         unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
@@ -69,10 +69,10 @@ int main() {
 
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glTexParameteri(texture2, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(texture2, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(texture2, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(texture2, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
         int width, height, nrChannels;
         unsigned char *data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
