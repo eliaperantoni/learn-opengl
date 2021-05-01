38c638a33baf673716a33fdd102005344ee0f2f1
Elia Perantoni <perantonielia0@gmail.com>
2021-05-01 11:08:55 +0200

    Exercise Lighting/BasicLighting/03

--- shaders/lighting/shader.fs
+++ shaders/lighting/shader.fs
@@ -1,32 +1,8 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-
-uniform vec3 lightPos;
-uniform vec3 viewPos;
-
-in vec3 FragPos;
-in vec3 Normal;
+in vec3 Color;
 
 void main() {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-
-    vec3 norm     = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    vec3 viewDir  = normalize(viewPos - FragPos);
-
-    float diff = max(dot(lightDir, norm), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    float specularStrength = 0.5;
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
-
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(Color, 1.0);
 }
