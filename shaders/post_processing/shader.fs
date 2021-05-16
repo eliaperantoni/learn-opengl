c301c59905ca0aceae79882797eef6c87756decc
Elia Perantoni <perantonielia0@gmail.com>
2021-05-16 18:50:03 +0200

    Sharpen

--- shaders/post_processing/shader.fs
+++ shaders/post_processing/shader.fs
@@ -5,8 +5,36 @@ out vec4 FragColor;
 
 uniform sampler2D sampler;
 
+const float offset = 1.0 / 300.0;
+
 void main() {
-    FragColor = texture(sampler, TexCoords);
-    float avg = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
-    FragColor = vec4(avg, avg, avg, 1.0);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right
+    );
+
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+
+    vec3 colors[9];
+    for(int i = 0; i < 9; i++) {
+        colors[i] = vec3(texture(sampler, TexCoords + offsets[i]));
+    }
+
+    vec3 color = vec3(0.0);
+    for(int i = 0; i < 9; i++) {
+        color += colors[i] * kernel[i];
+    }
+
+    FragColor = vec4(color, 1.0);
 }
