12ae382667e92cbaf254ae686bcb7ecd358fa896
Elia Perantoni <perantonielia0@gmail.com>
2021-06-17 13:05:21 +0200

    Environment map refraction (it looks like glass!)

--- shaders/emap/shader.fs
+++ shaders/emap/shader.fs
@@ -9,10 +9,12 @@ uniform vec3 cameraPos;
 uniform samplerCube skybox;
 
 void main() {
+    float refractionRatio = 1.00 / 1.52;
+
     vec3 viewDir = normalize(FragPos - cameraPos);
     vec3 normal = normalize(Normal);
 
-    vec3 reflectDir = reflect(viewDir, normal);
+    vec3 refractDir = refract(viewDir, normal, refractionRatio);
 
-    FragColor = vec4(texture(skybox, reflectDir).rgb, 1.0);
+    FragColor = vec4(texture(skybox, refractDir).rgb, 1.0);
 }
